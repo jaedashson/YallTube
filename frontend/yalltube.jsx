43c9d903204984024b21c3d891d35b3a9cc3731a
Jae-Son Song <jaesonsong@gmail.com>
2020-06-04 10:12:38 -0400

    Bootstrap current user

--- frontend/yalltube.jsx
+++ frontend/yalltube.jsx
@@ -8,8 +8,21 @@ import { login, signup, logout, getUserByEmail } from "./actions/session_actions
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
   const root = document.getElementById("root");
+  
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   // window testing
   window.login = login;
