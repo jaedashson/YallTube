70c81cf2e200f6f4a99b2e8be70c587ebc1bb5ef
Jae-Son Song <jaesonsong@gmail.com>
2020-06-08 20:21:10 -0400

    upload form

--- frontend/components/upload/upload_page.jsx
+++ frontend/components/upload/upload_page.jsx
@@ -5,6 +5,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 class UploadPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isUploaded: false
+    }
 
     this.handleUpload = this.handleUpload.bind(this);
   }
@@ -17,13 +20,30 @@ class UploadPage extends React.Component {
     return (
       <div className="upload-page">
         <div className="upload-container">
-          <button
-            onClick={(e) => e.preventDefault()} // placeholder click handler
-            className="upload-select-button"
-          >
-            <FontAwesomeIcon icon="upload" className="upload-icon"/>
-            <p>Select files to upload</p>
-          </button>
+          <form className="upload-form" >
+            <div className="upload-form-file-input-container">
+              <input type="file"
+                className="upload-form-file-input"
+              />
+            </div>
+            <div className="upload-form-inputs-container">
+              <div className="upload-form-title-container">
+                <input type="text"
+                  className="upload-form-title-input"
+                />
+              </div>
+              <div className="upload-form-description-container">
+                <textarea
+                  className="upload-form-description-input"
+                />
+              </div>
+            </div>
+            <div className="publish-button-container">
+              <button onClick={(e) => e.preventDefault()}
+                className="publish-button"
+              >Publish</button>
+            </div>
+          </form>
         </div>
       </div>
     );
