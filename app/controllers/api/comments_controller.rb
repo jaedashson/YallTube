25423b6cfced5be16a7bb6645fb40e48de2b5dfc
Jae-Son Song <jaesonsong@gmail.com>
2020-08-31 13:30:36 -0400

    write jbuilder view for show comment

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,12 +1,27 @@
 class Api::CommentsController < ApplicationController
 
-  # Get the 
+  # Get all the comments for this video
   # GET /api/videos/:video_id/comments
-  def
+  def index
     @comments = Comment.where(video_id: params[:video_id]) 
 
     render :index
   end
 
+  # Create a comment for this video
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render :show # What does this do?
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:author_id, :video_id, :parent_id, :text)
+  end
 
 end
---
+++ app/views/api/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :id, :author_id, :video_id, :parent_id, :body, :created_at, :updated_at
