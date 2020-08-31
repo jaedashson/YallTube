2b847b776832428e2cef2036dfe5b73788d23862
Jae-Son Song <jaesonsong@gmail.com>
2020-08-31 18:48:34 -0400

    save work

--- frontend/components/videos/comment_form.jsx
+++ frontend/components/videos/comment_form.jsx
@@ -8,25 +8,16 @@ class CommentForm extends React.Component {
       author_id: this.props.currentUser.id,
       video_id: this.props.videoId,
       body: "",
-      show: true,
     }
-  }
-
-  renderMain() {
-
-    return (
-      <div className="comment-form-main">
-
-      </div>
-    );
   }
 
   render() {
     return (
     <form className="comment-form">
       <div className="comment-form-icon">{this.props.currentUser.username[0]}</div>
-      {this.renderMain()}
-    </form>
+      <div className="comment-form-main">
+
+      </div>    </form>
     )
   }
 }
