d8d5c97d87f5f91be9c9cd52cb96bcadbe5baa99
Jae-Son Song <jaesonsong@gmail.com>
2020-09-02 03:10:31 -0400

    save work

--- frontend/actions/comments_actions.js
+++ frontend/actions/comments_actions.js
@@ -2,7 +2,8 @@ import * as APIUtil from "../util/comments_api_util";
 
 export const RECEIVE_PARENT_COMMENTS = "RECEIVE_PARENT_COMMENTS";
 export const RECEIVE_PARENT_COMMENT = "RECEIVE_PARENT_COMMENT";
-
+export const RECEIVE_REPLIES = "RECEIVE_REPLIES";
+export const RECEIVE_REPLY = "RECEIVE_REPLY";
 
 // POJO action creators
 
@@ -20,12 +21,32 @@ export const receiveParentComment = comment => {
   };
 };
 
+export const receiveReplies = comments => {
+  return {
+    type: RECEIVE_REPLIES,
+    comments
+  };
+};
+
+export const receiveReply = comment => {
+  return {
+    type: RECEIVE_REPLY,
+    comment
+  };
+};
+
 // Thunk action creators
 
 // TODO - Write error action creators
 export const fetchParentComments = videoId => dispatch => {
   return APIUtil.fetchParentComments(videoId).then(comments => {
     return dispatch(receiveParentComments(comments));
+  });
+};
+
+export const fetchReplies = commentId => dispatch => {
+  return APIUtil.fetchReplies(commentId).then(comments => {
+    return dispatch(receiveReplies(comments));
   });
 };
 
