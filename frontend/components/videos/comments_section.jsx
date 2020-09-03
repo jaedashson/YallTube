5ea201b6cafc8efdec92a782ef5b85df188f7d12
Jae-Son Song <jaesonsong@gmail.com>
2020-09-02 20:36:01 -0400

    commentForm is broken

--- frontend/components/videos/comments_section.jsx
+++ frontend/components/videos/comments_section.jsx
@@ -89,7 +89,7 @@ class CommentsSection extends React.Component {
           <button className="comment-sort-by">SORT BY</button>
         </div>
 
-        <CommentFormContainer currentUser={this.props.currentUser} videoId={this.props.videoId} />
+        {/* <CommentFormContainer currentUser={this.props.currentUser} videoId={this.props.videoId} /> */}
 
         <div className="comments-list">
           {this.renderParentComments()}
